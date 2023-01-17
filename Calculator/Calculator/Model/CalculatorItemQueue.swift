f976c7b86b82325a27636acd831da7db5395f59a
zhunhe <xuzhunhe@gmail.com>
2023-01-17 11:03:49 +0900

    feat: ✅ test_초기화했을때_isEmpty_true
    - 비어있는 protocol CalculateItem 구현
    - struct CalculatorItemQueue(items: [CalculateItem], isEmpty) 구현

---
+++ Calculator/Calculator/Model/CalculatorItemQueue.swift
@@ -0,0 +1,18 @@
+//
+//  CalculatorItemQueue.swift
+//  Calculator
+//
+//  Created by jun on 2023/01/17.
+//
+
+import Foundation
+
+protocol CalculateItem {}
+
+struct CalculatorItemQueue {
+    var items: [CalculateItem] = []
+
+    var isEmpty: Bool {
+        items.isEmpty
+    }
+}
