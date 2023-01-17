94c22d541e74bdf1a04c17105be933433628e1ee
sei <vanism7557@gmail.com>
2023-01-17 10:32:47 +0900

    test: test 모듈 추가

---
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -0,0 +1,21 @@
+//
+//  CalculatorTests.swift
+//  CalculatorTests
+//
+//  Created by sei on 2023/01/17.
+//
+
+import XCTest
+
+final class CalculatorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    func testExample() throws {
+    }
+
+}
