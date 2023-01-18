462c8f97d9cdd41265834d17843c42440a5e145c
zhunhe <xuzhunhe@gmail.com>
2023-01-18 16:07:18 +0900

    feat 🟢 test_parse에_빈문자열을_넣으면_빈_Formula를_반환한다
    - ExpressionParser.swift 생성

---
+++ Calculator/Calculator/Model/ExpressionParser.swift
@@ -0,0 +1,14 @@
+//
+//  ExpressionParser.swift
+//  Calculator
+//
+//  Created by jun on 2023/01/18.
+//
+
+import Foundation
+
+enum ExpressionParser {
+    static func parse(from input: String) -> Formula {
+        return Formula()
+    }
+}
