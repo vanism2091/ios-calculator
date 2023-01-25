c10cf31d0d5c519397b8f0c7de4e0da768616249
Sei Kim <vanism7557@gmail.com>
2023-01-25 12:46:35 +0900

    feat: equals, clear 버튼 ViewController와 연결

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -20,4 +20,12 @@ class ViewController: UIViewController {
         print(sender.currentTitle ?? "사칙연산")
     }
 
+    @IBAction func equalsDidTap(_ sender: UIButton) {
+        print(sender.currentTitle ?? "==")
+    }
+
+    @IBAction func clearDidTap(_ sender: UIButton) {
+        print(sender.currentTitle ?? "c")
+    }
+
 }
